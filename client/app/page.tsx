cff8814e693804c58a47a698979246ee1e97e5c5
YunFeng Zhang <73557467+fengzhang789@users.noreply.github.com>
2024-11-30 04:47:15 +0000

    changed husky pre-commit and formatted files

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -17,13 +17,18 @@ import { useMyPresence, useOthers } from "@liveblocks/react/suspense";
 import { Cursor } from "./components/Cursor";
 
 const nodeTypes = {
-  textUpdater: TextNode
-}
+  textUpdater: TextNode,
+};
 
 const initialNodes = [
   { id: "1", position: { x: 0, y: 0 }, data: { label: "1" }, type: "input" },
   { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
-  { id: "2", position: { x: 0, y: 200 }, data: { label: "2" }, type: "textUpdater" },
+  {
+    id: "2",
+    position: { x: 0, y: 200 },
+    data: { label: "2" },
+    type: "textUpdater",
+  },
 ];
 const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 
@@ -33,7 +38,7 @@ export default function Home() {
   const userCount = others.length;
 
   console.log(`There are ${userCount} other user(s) online`);
-  console.log(JSON.stringify(myPresence))
+  console.log(JSON.stringify(myPresence));
 
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
@@ -47,30 +52,31 @@ export default function Home() {
     <>
       <div
         onPointerMove={(e: React.PointerEvent<HTMLDivElement>) => {
-          const cursor = { x: Math.floor(e.clientX), y: Math.floor(e.clientY) }
-          updateMyPresence({ cursor })
+          const cursor = { x: Math.floor(e.clientX), y: Math.floor(e.clientY) };
+          updateMyPresence({ cursor });
         }}
         onPointerLeave={() => {
           const cursor = {
-            cursor: null
+            cursor: null,
           };
           updateMyPresence(cursor);
         }}
-        style={{ width: "100vw", height: "100vh" }}>
-          {others
-      .filter((other) => other.presence.cursor !== null)
-      .map(({ connectionId, presence }) => {
-        if (presence.cursor) {
-          console.log(`HI: ${JSON.stringify(presence.cursor)}`)
-          return  (
-            <Cursor
-              key={connectionId}
-              x={presence.cursor.x}
-              y={presence.cursor.y}
-            />
-          )
-        }
-      })}
+        style={{ width: "100vw", height: "100vh" }}
+      >
+        {others
+          .filter((other) => other.presence.cursor !== null)
+          .map(({ connectionId, presence }) => {
+            if (presence.cursor) {
+              console.log(`HI: ${JSON.stringify(presence.cursor)}`);
+              return (
+                <Cursor
+                  key={connectionId}
+                  x={presence.cursor.x}
+                  y={presence.cursor.y}
+                />
+              );
+            }
+          })}
         <ReactFlow
           nodes={nodes}
           edges={edges}
