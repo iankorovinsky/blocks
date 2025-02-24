0bc538e7c9a172ded2de8cf1ec0dcd01ef401928
Ian Korovinsky <ian.korovinsky@gmail.com>
2025-02-24 18:01:54 +0000

    Auth0 integrated

--- client/components/AuthProvider.tsx
+++ client/components/AuthProvider.tsx
@@ -8,7 +8,8 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN || ''}
       clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || ''}
       authorizationParams={{
-        redirect_uri: typeof window !== 'undefined' ? window.location.origin : ''
+        redirect_uri: window.location.origin,
+        audience: 'https://dev-xffaaa5fk8wi8c8m.us.auth0.com/api/v2/'
       }}
     >
       {children}
