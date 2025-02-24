32c9bc455e582593e2578e6eee7f4947e979896b
Ian Korovinsky <ian.korovinsky@gmail.com>
2025-02-24 18:05:46 +0000

    doing this for my build homies

--- client/components/AuthProvider.tsx
+++ client/components/AuthProvider.tsx
@@ -8,8 +8,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN || ''}
       clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || ''}
       authorizationParams={{
-        redirect_uri: window.location.origin,
-        audience: 'https://dev-xffaaa5fk8wi8c8m.us.auth0.com/api/v2/'
+        redirect_uri: typeof window !== 'undefined' ? window.location.origin : ''
       }}
     >
       {children}
